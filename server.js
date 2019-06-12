0b6860eff831e226259b1918916764a1a8cb52d6
Rafal Prykiel <rafalp92@o2.pl>
2019-06-12 21:33:30 +0200

    regular commit

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const core = express();
 
-const server = http.createServer((req, res) => {core});
+core.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+core.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+const server = http.createServer(core);
 
 server.listen(8000);
