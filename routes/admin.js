ece5c987a6af4dec369b69246632d80e432602cb
Rafal Prykiel <rafalp92@o2.pl>
2019-06-25 20:28:12 +0200

    regular commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const root = require('../utility/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
+    res.sendFile(path.join(root, 'views', 'add-product.html')); //__dirname, '..', 'views', 'add-product.html'
 });
 
 // /admin/add-product => POST
