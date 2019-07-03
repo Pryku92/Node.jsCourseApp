6c7b6617abf09ac9979673bbffd2208eddfc0d2f
Rafal Prykiel <rafalp92@o2.pl>
2019-07-03 19:44:20 +0200

    regular commit

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,10 @@ const root = require('../utility/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(root, 'views', 'shop.html')); // __dirname, '../', 'views', 'shop.html'
 });
 
