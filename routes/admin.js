3d7a3152fd335264d8f6895c2fc4aacc75e43b2c
Rafal Prykiel <rafalp92@o2.pl>
2019-07-19 11:46:19 +0200

    regular commit

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,6 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => GET
 router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
