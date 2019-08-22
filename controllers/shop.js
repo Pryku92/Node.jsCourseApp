a0249e5f5433d1e8f3d7d8c350f9b15545067407
Rafal Prykiel <rafalp92@o2.pl>
2019-08-22 07:37:33 +0200

    regular commit

--- controllers/shop.js
+++ controllers/shop.js
@@ -180,30 +180,41 @@ exports.postCartDeleteItem = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
+// // SEQUELIZE APPROACH
+// exports.postOrder = (req, res, next) => {
+//     let fetchedCart;
+//     req.user
+//         .getCart()
+//         .then(cart => {
+//             fetchedCart = cart;
+//             return cart.getProducts();
+//         })
+//         .then(products => {
+//             return req.user
+//                 .createOrder()
+//                 .then(order => {
+//                     return order.addProducts(
+//                         products.map(product => {
+//                             product.orderItem = {qty: product.cartItem.qty};
+//                             return product;
+//                         })
+//                     );
+//                 })
+//                 .catch(err => console.log(err));
+//         })
+//         .then(result => {
+//             return fetchedCart.setProducts(null);
+//         })
+//         .then(result => {
+//             res.redirect('/orders');
+//         })
+//         .catch(err => console.log(err));
+// }
+
 exports.postOrder = (req, res, next) => {
     let fetchedCart;
     req.user
-        .getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts();
-        })
-        .then(products => {
-            return req.user
-                .createOrder()
-                .then(order => {
-                    return order.addProducts(
-                        products.map(product => {
-                            product.orderItem = {qty: product.cartItem.qty};
-                            return product;
-                        })
-                    );
-                })
-                .catch(err => console.log(err));
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null);
-        })
+        .addOrder()
         .then(result => {
             res.redirect('/orders');
         })
